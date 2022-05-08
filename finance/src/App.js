bafece59f46a88a02c00410cbb4426d51928b525
Ben Evora <65423996+benevora@users.noreply.github.com>
2022-05-08 18:21:51 -0400

    page layout structuring

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Form from "./components/Form";
 import Header from "./components/Header"
+import Resume from "./components/Resume"
 import GlobalStyle from "./styles/global"
 
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
   return (
     <>
       <Header />
+      <Resume />
+      <Form />
       <GlobalStyle />
     </>
   );
